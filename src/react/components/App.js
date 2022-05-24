681b3f51feb6104eb25c72ebf19a51ab8fa971c4
Godwin Nwachukwu <nwgwin@gmail.com>
2022-05-24 14:39:30 +0100

    implement folder structure

---
+++ src/react/components/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import '../../assets/stylesheets/App.css';
+
+const App = () => {
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div className="app" data-testid="app-1">
+      <div className="header">
+        jkhnkjnkj
+      </div>
+    </div>
+  );
+};
+
+export default App;
